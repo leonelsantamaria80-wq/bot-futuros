0be6d6bb59ff0037686d23ca22c0454dfb0a9db9
leonelsantamaria80-wq <leonelsantamaria80@gmail.com>
2026-08-21 07:58:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import asyncio
 from flask import Flask
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -19,10 +20,13 @@ def run_flask():
     app.run(host='0.0.0.0', port=port)
 
 def run_bot():
+    # Esto arregla el error de MainThread
+    asyncio.set_event_loop(asyncio.new_event_loop())
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
 if __name__ == "__main__":
-    threading.Thread(target=run_flask).start()
+    # Flask va en hilo secundario
+    threading.Thread(target=run_flask, daemon=True).start()
     run_bot()
